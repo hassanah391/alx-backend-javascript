a0da207749ddb1f59fda3f785d35615339fd8d4d
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-04-19 01:43:34 +0200

    try map()

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, filename) {
-  const promises = [uploadPhoto(filename), signUpUser(firstName, lastName)];
-  const results = await Promise.allSettled(promises);
-  return results.map((o) => ({
-    status: o.status,
-    value: o.status === 'fulfilled' ? o.value : String(o.reason),
-  }));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
