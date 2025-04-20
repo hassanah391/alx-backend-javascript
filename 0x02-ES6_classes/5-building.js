0b52e7fbaffd3d8df984b27e05f8819a6113c093
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-04-20 20:01:24 +0200

    Applys abstraction concept

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,28 +1,40 @@
 /**
  * Represents a building with a specific square footage.
  *
- * This class is intended to be extended by other classes.
- * Subclasses must override the `evacuationWarningMessage` method.
+ * This class serves as a base class for other building types.
+ * Subclasses must implement the `evacuationWarningMessage` method.
  */
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft === 'number') {
-      this._sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
-  }
-
-  get sqft() {
-    return this._sqft;
+    this._sqft = sqft;
   }
 
   /**
-   * Placeholder method for evacuation warning messages.
+   * Sets the square footage of the building.
    *
-   * @throws {Error} If the method is not overridden by a subclass.
+   * @param {number} sqft - The square footage of the building.
+   * @throws {TypeError} If sqft is not a number.
    */
-  evacuationWarningMessage() {
-    if (this.evacuationWarningMessage === Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
     }
+    this._sqft = sqft;
+  }
+
+  /**
+   * Gets the square footage of the building.
+   *
+   * @returns {number} The square footage of the building.
+   */
+  get sqft() {
+    return this._sqft;
   }
 }
