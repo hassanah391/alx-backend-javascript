30b6a620a367c03c61831ee5eafdfe965e16c22a
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-04-24 00:22:42 +0200

    Corrects id of 'Serena' student

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -20,7 +20,7 @@ export default function getListStudents() {
       location: 'Columbia',
     },
     {
-      id: 3,
+      id: 5,
       firstName: 'Serena',
       location: 'San Francisco',
     },
