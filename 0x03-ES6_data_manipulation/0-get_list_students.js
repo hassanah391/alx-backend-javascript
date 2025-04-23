a077bd050a733fc2a6915f20e6d6999709a56e65
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-04-24 00:18:45 +0200

    Adds getListStudents() that return list of Students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,30 @@
+/**
+ * Returns an array of student objects.
+ * Each student object contains the following properties:
+ * - id: A unique identifier for the student (number).
+ * - firstName: The first name of the student (string).
+ * - location: The location of the student (string).
+ *
+ * @returns {Array<Object>} Array of student objects.
+ */
+export default function getListStudents() {
+  const listStudents = [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 3,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+
+  return listStudents;
+}
