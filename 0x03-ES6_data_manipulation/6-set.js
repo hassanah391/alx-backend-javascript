2c22b0894a354906224295d32671588757536351
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-04-24 07:13:33 +0200

    Adds setFromArray()

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Converts an array to a set.
+ *
+ * @param {*Array} array - an array of elements
+ * @returns {*Set}- a Set from an array.
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
