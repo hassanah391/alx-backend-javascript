c4aa6eb3628b2866c5bf6cc2dd79072c700aabbb
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-04-24 17:33:58 +0200

    Adds a docstring for groceriesList()

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,3 +1,9 @@
+/**
+ * Creates a map of grocery items with their quantities.
+ *
+ * @returns {Map<string, number>} - A Map where the keys are grocery item names (strings)
+ * and the values are their quantities (numbers).
+ */
 export default function groceriesList() {
   const groceries = new Map([
     ['Apples', 10],
