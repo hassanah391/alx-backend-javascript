2e1bd61d34ea6c573433993757182d053c690e49
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-06-10 11:19:02 +0300

    Remove process.exit()

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,6 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.trim()}`);
-  process.exit();
 });
 
 process.on('exit', () => {
