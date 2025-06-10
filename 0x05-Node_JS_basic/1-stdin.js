dcd309c1c69bda3fcf95e79301a2efe17e4b2261
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-06-10 11:05:39 +0300

    Add script 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+process.stdin.setEncoding('utf8');
+
+console.log('Welcome to ALX, what is your name?');
+
+process.stdin.on('data', (data) => {
+  console.log(`Your name is: ${data.trim()}`);
+});
+
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
+});
