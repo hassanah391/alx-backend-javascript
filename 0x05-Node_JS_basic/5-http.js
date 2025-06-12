c283873e4cb067c198874eb9d3d13223b581fb77
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-06-12 09:29:47 +0300

    Changes ALX to Holberton School

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -39,7 +39,7 @@ const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   if (req.url === '/') {
-    res.end('Hello ALX!');
+    res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
 
