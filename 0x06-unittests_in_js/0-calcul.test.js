4c885ee9e4d123f13008921ac14a56e557e474aa
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-06-13 13:16:38 +0300

    Add some tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,7 +3,7 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => { 
   it('should return sum when adding two integers', () => {
-    assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1.0, 3), 4);
   });
 
   it('should round second argument before adding', () => {
@@ -20,5 +20,13 @@ describe('calculateNumber', () => {
 
   it('should round up when decimal part is .5 or greater', () => {
     assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('rounding up a\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
+  });
+
+  it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
   });
 });
